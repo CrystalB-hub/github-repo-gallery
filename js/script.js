921543e9965f41398dbd32a0643ce27782833c98
crystal_b <crystal.beecken@gmail.com>
2022-07-29 16:11:41 +0900

    Added async function to pull repo data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Target profile info
 const profile = document.querySelector (".overview");
 const username = "CrystalB-hub";
+const repoList = document.querySelector (".repo-list");
 
 const gitHub = async function () {
     const res = await fetch (
@@ -28,4 +29,25 @@ const displayUserInfo = function (user) {
     </div> 
    `;
     profile.append(div);
+    fetchRepos();
+};
+
+const fetchRepos = async function () {
+    const getRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData= await getRepos.json();
+    //console.log(repos);
+    displayRepoInfo(repoData);
+};
+
+
+//fetchRepos();
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const item = document.createElement ("li");
+        item.classList.add("repo");
+        item.innerHTML = `
+        <h3>${repo.name}</h3>
+        `;
+        repoList.append(item);   
+} 
 };
