f3fb4635b1c2f26cbcb3d244434484210fdb2b9c
crystal_b <crystal.beecken@gmail.com>
2022-08-01 19:08:09 +0900

    Added click event to display repo details

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const profile = document.querySelector (".overview");
 const username = "CrystalB-hub";
 const repoList = document.querySelector (".repo-list");
+const repos = document.querySelector (".repos");
+const repoData = document.querySelector (".repo-data");
 
 const gitHub = async function () {
     const res = await fetch (
@@ -51,3 +53,49 @@ const displayRepoInfo = function (repos) {
         repoList.append(item);   
 } 
 };
+
+//Click event for repo info
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    specificRepoData(repoName);
+    }
+});
+
+//Fetch specific repo info
+const specificRepoData = async function (repoName) {
+    const res = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo= await res.json();
+    console.log(repoInfo);
+    //get languages
+    const fetchLanguages = await fetch (repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+    console.log(languageData);
+
+    //add to languages array
+    const languages = [];
+    for (const language in languageData) {
+        languages.push(language);
+        console.log(languages);
+    }
+
+    showRepoInfo(repoInfo, languages);
+};
+
+//Display repo info function
+const showRepoInfo = function(repoInfo, languages) {
+    repoData.innerHTML = "";
+    repoData.classList.remove("hide");
+    repos.classList.add("hide");
+    const div = document.createElement ("div");
+    div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+
+    repoData.append(div);
+};
+
+
